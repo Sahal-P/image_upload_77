20ed57bd38ab6b206d0813644e9b62d1f3d4c502
Sahal P <sahlcyprus@gmail.com>
2024-08-10 10:56:33 +0530

    feat: added model select option

---
+++ src/axios/api.js
@@ -0,0 +1,3 @@
+export const GET_MODELS_API = "http://20.84.124.151/api/models";
+
+export const POST_RUN_IMAGE = "http://20.84.124.151/api/image";
--- src/components/ImageUploadPreview.jsx
+++ src/components/ImageUploadPreview.jsx
@@ -1,10 +1,10 @@
 import { CircularProgress } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
-import { JsonView, allExpanded, defaultStyles } from 'react-json-view-lite';
-import 'react-json-view-lite/dist/index.css';
+import { useEffect, useState } from "react";
+import { JsonView, allExpanded, defaultStyles } from "react-json-view-lite";
+import "react-json-view-lite/dist/index.css";
 import { toast } from "react-toastify";
-
+import { GET_MODELS_API, POST_RUN_IMAGE } from "../axios/api";
 
 const ImageUploadPreview = () => {
   const [image, setImage] = useState(null);
@@ -12,7 +12,26 @@ const ImageUploadPreview = () => {
   const [loading, setLoading] = useState(false);
   const [outputImage, setOutputImage] = useState(null);
   const [jsonResult, setJsonResult] = useState(null);
+  const [models, setModels] = useState([]);
+  const [selectedModel, setSelectedModel] = useState(""); // Default model name
 
+  useEffect(() => {
+    const fetchModels = async () => {
+      try {
+        const response = await axios.get(GET_MODELS_API);
+        setModels(response?.data?.models); // Assuming the API returns a list of models in 'models' field
+        handleModelSelect(response?.data?.models?.[0]?.model);
+      } catch (error) {
+        console.error("Error fetching models:", error);
+      }
+    };
+
+    fetchModels();
+  }, []);
+
+  const handleModelSelect = (model) => {
+    setSelectedModel(model);
+  };
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -30,7 +49,7 @@ const ImageUploadPreview = () => {
     setImage(null);
     setPreview(null);
     setJsonResult(null);
-    setOutputImage(null)
+    setOutputImage(null);
   };
 
   const handleUploadImage = async () => {
@@ -48,15 +67,12 @@ const ImageUploadPreview = () => {
       const base64String = reader.result.split(",")[1];
 
       const payload = {
-        model_name: "Yolo",
+        model_name: selectedModel,
         image_bytes: base64String,
       };
 
       try {
-        const response = await axios.post(
-          "http://20.84.124.151/api/image",
-          payload
-        );
+        const response = await axios.post(POST_RUN_IMAGE, payload);
         const { output_image, result } = response.data;
 
         // Convert the output_image from base64 to a displayable format
@@ -64,13 +80,15 @@ const ImageUploadPreview = () => {
 
         // Set the JSON result
         setJsonResult(result);
-        toast.success('Image uploaded successfully!');
-        console.log("Upload successful:", response.data);
-        
+        toast.success("Image uploaded successfully!");
       } catch (error) {
-        toast.error('Failed to upload image. Please try again.');
+        if (error?.response?.status === 500) {
+          toast.error(error?.message);
+        } else {
+          toast.error("Failed to upload image. Please try again.");
+        }
+
         setLoading(false);
-        console.error("Error uploading image:", error);
       } finally {
         setLoading(false); // Stop loading
       }
@@ -78,6 +96,24 @@ const ImageUploadPreview = () => {
   };
   return (
     <div className="flex flex-col items-center mt-8 w-full mb-56">
+      <div className="flex flex-col mb-4">
+        <h2 className="text-lg font-bold mb-2 mt-5">Test models:</h2>
+        <div>
+          {models?.map((data) => (
+            <button
+              key={data?.model}
+              onClick={() => handleModelSelect(data?.model)}
+              className={`mr-4 py-2 px-4 rounded ${
+                selectedModel === data?.model
+                  ? "bg-blue-700 text-white"
+                  : "bg-gray-300 text-gray-700"
+              }`}
+            >
+              {data?.model}
+            </button>
+          ))}
+        </div>
+      </div>
       <div className="w-96 h-96 flex items-center justify-center bg-gray-100 rounded-md border border-gray-300">
         {preview ? (
           <img
@@ -116,11 +152,10 @@ const ImageUploadPreview = () => {
               className="ml-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
             >
               {loading ? (
-                <CircularProgress size={20} sx={{color: "white"}} />
+                <CircularProgress size={20} sx={{ color: "white" }} />
               ) : (
                 "Run"
               )}
-
             </button>
           </>
         )}
@@ -128,13 +163,13 @@ const ImageUploadPreview = () => {
       {outputImage && (
         <>
           <h2 className="text-lg font-bold mb-2 mt-5">Processed Image:</h2>
-        <div className="mt-8 w-96 h-96 flex items-center justify-center bg-gray-100 rounded-md border border-gray-300">
-          <img
-            src={outputImage}
-            alt="Processed Output"
-            className="w-full h-full object-contain rounded-md"
-          />
-        </div>
+          <div className="mt-8 w-96 h-96 flex items-center justify-center bg-gray-100 rounded-md border border-gray-300">
+            <img
+              src={outputImage}
+              alt="Processed Output"
+              className="w-full h-full object-contain rounded-md"
+            />
+          </div>
         </>
       )}
 
@@ -142,9 +177,13 @@ const ImageUploadPreview = () => {
       {jsonResult && (
         <div className="mt-8 w-[90%]">
           <h2 className="text-lg font-bold mb-2">Results:</h2>
-          <JsonView data={jsonResult} shouldExpandNode={allExpanded} style={defaultStyles} />
+          <JsonView
+            data={jsonResult}
+            shouldExpandNode={allExpanded}
+            style={defaultStyles}
+          />
         </div>
-      )}    
+      )}
     </div>
   );
 };
